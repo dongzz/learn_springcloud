56b972cfce6c7bc74ca8f63de760c155b6c41eb9
dongz <89518029@qq.com>
2020-04-24 14:44:10 +0800

     支付模块

---
+++ host/payment/src/main/java/com/dongz/springcloud/PaymentApplication.java
@@ -0,0 +1,16 @@
+package com.dongz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dong
+ * @date 2020/4/24 14:41
+ * @desc
+ */
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
